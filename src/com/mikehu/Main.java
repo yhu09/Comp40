38700c610e526afa7e23d8ab2c45a0c7aade48ca
Mike Hu <mikehu@Mikes-MBP.fios-router.home>
2019-06-16 16:26:48 -0400

    First Commit

---
+++ src/com/mikehu/Main.java
@@ -0,0 +1,12 @@
+package com.mikehu;
+
+public class Main {
+	public static void main(String[] args ) {
+//		double val = 20;
+//		double secVal = 80;
+//		double result = (val + secVal) * 25;
+//		double remainder = result % 40;
+//		if(remainder <=20) System.out.print("Total was over the limit");
+		System.out.println("Hello World");
+	}
+} 
